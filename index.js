c7249be2f989ed11635d4907570eb2b7d5ca24e1
Jeff Sulfranc <sulfranc@gmail.com>
2021-12-08 14:36:06 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(array, string){
+    let arr = [];
+for (let i = 0; i < array.length; i++){
+    arr[i] = "Thank you, " + array[i] + ", for the wonderful " + string + " gift!";
+}
+return arr;
+}
+
+writeCards();
+
+function countDown(count){
+while (count >= 0){
+    console.log(count--)
+}
+}
+countDown(10);
